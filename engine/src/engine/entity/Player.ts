6063cc62ef0e53786347a6d56a343ea562ecd66b
gradwahl <argnara@gmail.com>
2026-05-20 18:02:17 +0100

    Revert "AFK Update"
    This reverts commit 64b15cea7e72b286320056ca37855a185fa11f4c.

--- engine/src/engine/entity/Player.ts
+++ engine/src/engine/entity/Player.ts
@@ -1918,13 +1918,7 @@ export default class Player extends PathingEntity {
             return;
         }
 
-        let multi = allowMulti ? Environment.NODE_XPRATE : 1;
-        if (allowMulti && Environment.NODE_PROGRESSIVE_XP) {
-            // Square-root curve: level 1 = 1x, level 99 = NODE_PROGRESSIVE_XP_SCALE x.
-            const currentLevel = this.baseLevels[stat];
-            const scale = Environment.NODE_PROGRESSIVE_XP_SCALE;
-            multi *= 1 + Math.sqrt((currentLevel - 1) / 98) * (scale - 1);
-        }
+        const multi = allowMulti ? Environment.NODE_XPRATE : 1;
         this.stats[stat] += xp * multi;
 
         // cap to 200m, this is represented as "2 billion" because we use 32-bit signed integers and divide by 10 to give us a decimal point
