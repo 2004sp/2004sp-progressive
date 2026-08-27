5a763ff714b8233de6213742d98c87e620265bae
gradwahl <argnara@gmail.com>
2026-08-27 04:20:11 +0100

    build(webclient): bundle scrollwheel zoom entrypoint

--- webclient/bundle.ts
+++ webclient/bundle.ts
@@ -64,6 +64,7 @@ async function applyTerser(script: BunOutput): Promise<boolean> {
                     'middleMouseRotation',
                     'compassReset',
                     'antiMacroRotation',
+                    'scrollwheelZoom',
 
                     // stdlib
                     'willReadFrequently',
@@ -138,13 +139,11 @@ const args = process.argv.slice(2);
 const prod = args[0] !== 'dev';
 
 const entrypoints = [
-    'src/client/Client.ts',
-    'src/mapview/MapView.ts'
+    { file: 'src/client/ClientEntry.ts', output: 'client.js' },
+    { file: 'src/mapview/MapView.ts', output: 'mapview.js' }
 ];
 
-for (const file of entrypoints) {
-    const output = path.basename(file).replace('.ts', '.js').toLowerCase();
-
+for (const { file, output } of entrypoints) {
     const script = await bunBuild(file, [], prod, prod ? ['console'] : []);
     if (script) {
         if (prod) {
