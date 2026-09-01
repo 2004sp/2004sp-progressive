28789b276bb73b046bb4987d20fc9950e0e0847a
gradwahl <argnara@gmail.com>
2026-09-01 12:54:55 +0100

    Backport complete r481 GE interface sprite set

--- engine/grand-exchange-stage.ts
+++ engine/grand-exchange-stage.ts
@@ -1,4 +1,5 @@
 import { prepareGrandExchangeStage as prepareGrandExchangeBaseStage, restoreGrandExchangeStage } from './grand-exchange-stage-base.js';
+import { prepareGrandExchangeSpriteStage } from './grand-exchange-sprite-stage.js';
 import { prepareGrandExchangeGroup106Stage } from './grand-exchange-group106-stage.js';
 import { prepareGrandExchangeGroup107Stage } from './grand-exchange-group107-stage.js';
 import { prepareGrandExchangeGroup108Stage } from './grand-exchange-group108-stage.js';
@@ -12,6 +13,7 @@ export async function prepareGrandExchangeStage() {
     const stagedContentDir = await prepareGrandExchangeBaseStage();
 
     try {
+        await prepareGrandExchangeSpriteStage(stagedContentDir);
         await prepareGrandExchangeGroup106Stage(stagedContentDir);
         await prepareGrandExchangeGroup107Stage(stagedContentDir);
         await prepareGrandExchangeGroup108Stage(stagedContentDir);
