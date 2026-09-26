d378088c212df711eec7b99740bfd2761fb9dcae
gradwahl <argnara@gmail.com>
2026-09-26 17:07:22 +0100

    Run updater before Compact Manager on Windows

--- Run.bat
+++ Run.bat
@@ -5,8 +5,9 @@ title 2004Scape Compact Manager
 where node >nul 2>&1
 if errorlevel 1 (
   echo Node.js is required to run 2004Scape.
-  echo Install Node.js and run PLAY.bat again.
+  echo Install Node.js and run Run.bat again.
   pause
   exit /b 1
 )
+node launcher\updater.js
 node launcher\launcher.js
